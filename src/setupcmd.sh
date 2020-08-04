605756a6eddd6be602358bfef24ff76ac3add01f
vansteki <vansteki.tw@gmail.com>
2020-08-04 21:03:33 +0800

    fix duplicated setup alias

--- src/setupcmd.sh
+++ src/setupcmd.sh
@@ -2,13 +2,16 @@
 
 function setup() {
   echo 'setup...'
-  printf "\n# My dotfiles helper\n" >>~/.zshrc
-  echo alias dotfiles=\"bash $(pwd)/src/dotfiles.sh\" >>~/.zshrc
   result=$(cat ~/.zshrc | grep -c dotfiles.sh)
+
   if [[ $result -ge 0 ]]; then
     echo 'dotfiles alias in ~/.zshrc founded:'
     cat ~/.zshrc | grep dotfiles.sh
+  else
+    printf "\n# My dotfiles helper\n" >>~/.zshrc
+    echo alias dotfiles=\"bash $(pwd)/src/dotfiles.sh\" >>~/.zshrc
   fi
+
   echo "usage: use 'dotfiles update' to update your dotfiles"
 }
 
