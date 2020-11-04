070b5e7158bc620f32f2eb996d1c3a5f185bd9b3
vansteki <vansteki.tw@gmail.com>
2020-11-04 13:12:48 +0800

    update dotfiles

--- src/dotfiles.sh
+++ src/dotfiles.sh
@@ -5,7 +5,7 @@ PWD=$(dirname $0)
 DOTFILES_DIR=${PWD%/*}/dotfiles/macos
 
 source $PWD/update.sh
-if [[ $1 == "push" ]]; then
+if [[ $1 == "push" && $2 == "--all" ]]; then
   echo ---
   echo "update dotfiles..."
   echo ---
